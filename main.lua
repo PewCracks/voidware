dfec7e41c38d1c296edb2783749f42ad31d39bdb
Pew <124732041+PewCracks@users.noreply.github.com>
2025-10-21 03:10:13 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,39 @@
+if isfunctionhooked(game.HttpGet) then
+    restorefunction(game.HttpGet)
+end;
+
+local baseUrl = "https://raw.githubusercontent.com/PewCracks/voidware/refs/heads/main/";
+local hosted = {
+    "VWUpdateLog.lua",
+    "Helper.lua",
+    "Languages.json",
+    "NightsInTheForest.json",
+    "hash.lua",
+    "obfuscated.lua"
+};
+
+local old;
+old = hookfunction(game.HttpGet, newcclosure(function(self, url, ...)
+    local parts = string.split(url, "/")
+
+    -- UI
+    local uiLibrary = string.find(url, "Obsidian") and "Obsidian" or string.find(url, "LinoriaLib") and "Linoria" or false;
+    if uiLibrary then
+        return old(self, string.format("%s%s/%s", baseUrl, uiLibrary, parts[#parts]))
+    end;
+    
+    -- Whitelist
+    if url == "https://whitelist.vapevoidware.xyz" then
+        return [[{"Announcement":{"expiretime":1971012321.4514153,"targets":"all","text":"Pew was here"},"BlacklistedUsers":{},"KillVape":false,"WhitelistedTags":{},"WhitelistedUsers":{}}]];
+    elseif string.find(url, "hashsearch") then
+        return [[{"123":{"attackable":false,"hash":"CrackedByPew","level":2,"hidetag":false,"tags":[{"text":"VOIDWARE PRIVATE","color":[210,43,43]}]}}]];
+    end;
+
+    if table.find(hosted, parts[#parts]) then
+        return old(self, baseUrl .. parts[#parts])
+    end;
+
+    return old(self, url, ...)
+end))
+
+loadstring(game.HttpGet(game, "Pew was here/obfuscated.lua"))()
